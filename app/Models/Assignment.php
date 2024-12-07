35ecca2217178e1bc3febbdb48a8e9e1a8a72031
LeaOhyeah <leasehat@gmail.com>
2024-12-07 14:37:25 +0800

    add: adding model assignment

---
+++ app/Models/Assignment.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Assignment extends Model
+{
+    /** @use HasFactory<\Database\Factories\AssignmentFactory> */
+    use HasFactory;
+    protected $guarded = ['id'];
+
+    public function topic()
+    {
+        return $this->belongsTo(Topic::class);
+    }
+    public function classroom()
+    {
+        return $this->belongsTo(Classroom::class);
+    }
+    
+    // public function studentAnswer(): HasMany
+    // {
+    //     return $this->hasMany(StudentTaskAnswerCollection::class);
+    // }
+
+    // public function assignmentChat()
+    // {
+    //     return $this->hasMany(AssignmentChat::class);
+    // }
+}
---
+++ database/migrations/2024_12_07_062606_create_assignments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Enums\AssignmentStatusEnum;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('assignments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('classroom_id')->constrained('classrooms')->onDelete('cascade');
+            $table->foreignId('topic_id')->constrained('topics')->onDelete('cascade');
+            $table->string('title');
+            $table->text('content')->nullable();
+            $table->dateTime('due_date');
+            $table->integer('max_score')->default(100);
+            $table->enum('status', AssignmentStatusEnum::toArray())->default(AssignmentStatusEnum::ACTIVE);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('assignments');
+    }
+};
