1cc579e5f30c5dbed9bd4dde7f2233853afe890a
LeaOhyeah <leasehat@gmail.com>
2024-12-15 16:45:30 +0800

    update: updating messages error assignment chat request

--- app/Http/Requests/AssignmentChatRequest.php
+++ app/Http/Requests/AssignmentChatRequest.php
@@ -30,4 +30,21 @@ class AssignmentChatRequest extends FormRequest
             'message' => 'required|string'
         ];
     }
+
+     /**
+     * Custom messages for validation errors.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'assignment_id.required' => 'ID tugas wajib diisi.',
+            'assignment_id.exists' => 'ID tugas tidak ditemukan.',
+            'user_id.required' => 'ID pengguna wajib diisi.',
+            'user_id.exists' => 'ID pengguna tidak ditemukan.',
+            'message.required' => 'Pesan wajib diisi.',
+            'message.string' => 'Pesan harus berupa teks.',
+        ];
+    }
 }
